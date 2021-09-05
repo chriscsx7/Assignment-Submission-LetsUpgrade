3ae16b6652f4b7719887028e41fe077f7f4966c2
chriscsx7 <83710985+chriscsx7@users.noreply.github.com>
2021-09-05 04:47:58 -0500

    Assignment 1 submission

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+# 1. Add 2 numbers
+a = 10
+b = 25
+
+print(a+b)
+
+# 2. Try to add integer with string and see the output
+c = '15'
+
+# print(a+c) This line makes an error by the different data types
+print(a+int(c))
+
+# 3.Print variable and strings in one line with the help of f string
+print(f"The c variable is: {c}")
+
+# 4.How to add space and new line in the print statement
+print("This is a \
+simple string")
+print("This is a \nsimple string")
